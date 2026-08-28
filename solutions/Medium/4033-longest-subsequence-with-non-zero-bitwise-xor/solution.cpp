78a981a6c43e544a249a9424222cd76d1c347722
Sourav Arya <152316222+imsouravarya@users.noreply.github.com>
2026-08-29 01:09:46 +0530

    Add solution for LeetCode #4033 (C++)

---
+++ solutions/Medium/4033-longest-subsequence-with-non-zero-bitwise-xor/solution.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+
+class Solution {
+public:
+    int longestSubsequence(std::vector<int>& nums) {
+        int totalXor = 0;
+        bool hasNonZero = false;
+        
+
+        for (int num : nums) {
+            totalXor ^= num;
+            if (num != 0) {
+                hasNonZero = true;
+            }
+        }
+        
+ 
+        if (totalXor != 0) {
+            return nums.size();
+        }
+        
+  
+        if (hasNonZero) {
+            return nums.size() - 1;
+        }
+        
+     
+        return 0;
+    }
+};
